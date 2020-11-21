bf21f20b8ed7eda518cdb1a39bf1345e184eecf6
Rahul <rahul.cocoalabs.com>
2020-11-21 10:27:19 +0530

    first push

---
+++ config.js
@@ -0,0 +1,100 @@
+module.exports = {
+	port:8000,
+	routes:  [
+		{
+			path:"books",
+			port:"7000"
+		},
+		{
+			path:"videos", 
+			port:"7001"
+		}, 
+		{
+			path:"games", 
+			port:"7002"
+		}, 
+		{
+			path:"charities", 
+			port:"7003"
+		}, 
+		{
+			path:"tests", 
+			port:"7004"
+		}, 
+		{
+			path:"feeds", 
+			port:"7005"
+		},  
+		{
+			path:"accounts", 
+			port:"7006"
+		},
+		{
+			path: "ads",
+			port: "7007"
+		},
+		{
+			path: "reviews",
+			port: "7008"
+		},
+		{
+			path: "events",
+			port: "7009"
+		},
+		{
+			path: "bookings",
+			port: "7010"
+		},
+		{
+			path: "movies",
+			port: "7011"
+		},
+		{
+			path: "store",
+			port: "7012"
+		},
+		{
+			path: "masters",
+			port: "7013"
+		},
+		{
+			path: "favourites",
+			port: "7014"
+		},
+		{
+			path: "contact",
+			port: "7015"
+		},
+		{
+			path: "notifications",
+			port: "7016"
+		},
+		{
+			path:"theatre",
+			port:"7017"
+		},
+		{
+			path:"address",
+			port:"7018"
+		},
+		{
+			path:"cart",
+			port:"7019"
+		},
+		{
+			path:"contest",
+			port:"7020"
+		},
+		{
+			path:"shop-products",
+			port:"7021"
+		},
+		{
+			path:"online-class",
+			port:"7022"
+		}
+
+
+	]
+
+}
