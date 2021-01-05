92d2746317ee2612b4b2a9946918b4f788898e8d
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-01-05 18:25:35 +0530

    D

--- config.js
+++ config.js
@@ -1,104 +1,212 @@
 module.exports = {
-	port:8000,
+	//port:8000,
+	port:6300,
 	routes:  [
 		{
 			path:"books",
-			port:"7000"
+			port:"6301"
 		},
 		{
 			path:"videos", 
-			port:"7001"
+			port:"6302"
 		}, 
 		{
 			path:"games", 
-			port:"7002"
+			port:"6303"
 		}, 
 		{
 			path:"charities", 
-			port:"7003"
+			port:"6304"
 		}, 
 		{
 			path:"tests", 
-			port:"7004"
+			port:"6305"
 		}, 
 		{
 			path:"feeds", 
-			port:"7005"
+			port:"6306"
 		},  
 		{
 			path:"accounts", 
-			port:"7006"
+			port:"6307"
 		},
 		{
 			path: "ads",
-			port: "7007"
+			port: "6308"
 		},
 		{
 			path: "reviews",
-			port: "7008"
+			port: "6309"
 		},
 		{
 			path: "events",
-			port: "7009"
+			port: "6310"
 		},
 		{
 			path: "bookings",
-			port: "7010"
+			port: "6311"
 		},
 		{
 			path: "movies",
-			port: "7011"
+			port: "6312"
 		},
 		{
 			path: "store",
-			port: "7012"
+			port: "6313"
 		},
 		{
 			path: "masters",
-			port: "7013"
+			port: "6314"
 		},
 		{
 			path: "favourites",
-			port: "7014"
+			port: "6315"
 		},
 		{
 			path: "contact",
-			port: "7015"
+			port: "6316"
 		},
 		{
 			path: "notifications",
-			port: "7016"
+			port: "6317"
 		},
 		{
 			path:"theatre",
-			port:"7017"
+			port:"6318"
 		},
 		{
 			path:"address",
-			port:"7018"
+			port:"6319"
 		},
 		{
 			path:"cart",
-			port:"7019"
+			port:"6320"
 		},
 		{
 			path:"contest",
-			port:"7020"
+			port:"6321"
 		},
 		{
 			path:"shop-products",
-			port:"7021"
+			port:"6322"
 		},
 		{
 			path:"online-class",
-			port:"7022"
+			port:"6323"
 		},
 		{
 			path:"tutor",
-			port:"7023"
+			port:"6324"
 		}
 
 
 	]
 
 }
+
+
+// module.exports = {
+// 	//port:8000,
+// 	port:6300,
+// 	routes:  [
+// 		{
+// 			path:"books",
+// 			port:"7000"
+// 		},
+// 		{
+// 			path:"videos", 
+// 			port:"7001"
+// 		}, 
+// 		{
+// 			path:"games", 
+// 			port:"7002"
+// 		}, 
+// 		{
+// 			path:"charities", 
+// 			port:"7003"
+// 		}, 
+// 		{
+// 			path:"tests", 
+// 			port:"7004"
+// 		}, 
+// 		{
+// 			path:"feeds", 
+// 			port:"7005"
+// 		},  
+// 		{
+// 			path:"accounts", 
+// 			port:"7006"
+// 		},
+// 		{
+// 			path: "ads",
+// 			port: "7007"
+// 		},
+// 		{
+// 			path: "reviews",
+// 			port: "7008"
+// 		},
+// 		{
+// 			path: "events",
+// 			port: "7009"
+// 		},
+// 		{
+// 			path: "bookings",
+// 			port: "7010"
+// 		},
+// 		{
+// 			path: "movies",
+// 			port: "7011"
+// 		},
+// 		{
+// 			path: "store",
+// 			port: "7012"
+// 		},
+// 		{
+// 			path: "masters",
+// 			port: "7013"
+// 		},
+// 		{
+// 			path: "favourites",
+// 			port: "7014"
+// 		},
+// 		{
+// 			path: "contact",
+// 			port: "7015"
+// 		},
+// 		{
+// 			path: "notifications",
+// 			port: "7016"
+// 		},
+// 		{
+// 			path:"theatre",
+// 			port:"7017"
+// 		},
+// 		{
+// 			path:"address",
+// 			port:"7018"
+// 		},
+// 		{
+// 			path:"cart",
+// 			port:"7019"
+// 		},
+// 		{
+// 			path:"contest",
+// 			port:"7020"
+// 		},
+// 		{
+// 			path:"shop-products",
+// 			port:"7021"
+// 		},
+// 		{
+// 			path:"online-class",
+// 			port:"7022"
+// 		},
+// 		{
+// 			path:"tutor",
+// 			port:"7023"
+// 		}
+
+
+// 	]
+
+// }
